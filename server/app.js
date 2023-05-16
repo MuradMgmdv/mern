fdb4cdfc845fd53be11f336ffb8daec6a712b0c0
Murad <murad186992@mail.ru>
2023-05-16 18:40:54 +0300

    init-jwt

--- server/app.js
+++ server/app.js
@@ -1,7 +1,10 @@
 import express from 'express';
+import jwt from 'jsonwebtoken';
 const app = express();
+
 const PORT = 3001;
 
+// для того чтобы бэк смог прочитать json который придет с фронта
 app.use(express.json());
 
 app.get('/', (req, res) => {
@@ -10,8 +13,18 @@ app.get('/', (req, res) => {
 
 app.post('/auth/login', (req, res) => {
   //   console.log(req.body);
+
+  // генерируем токен, зашифровываем значения которые нам пришлет пользователь
+  const token = jwt.sign(
+    {
+      email: req.body.email,
+      fullName: 'Дональд Трамп',
+    },
+    'secret123',
+  );
   res.json({
     success: true,
+    token,
   });
 });
 
