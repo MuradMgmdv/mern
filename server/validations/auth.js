0a8a2e92ad17e6cb7c256807d8d6c42c90fa6895
Murad <murad186992@mail.ru>
2023-06-01 12:41:23 +0300

    validator-correction

--- server/validations/auth.js
+++ server/validations/auth.js
@@ -1,8 +1,8 @@
 import { body } from 'express-validator';
 
 export const registerValidation = [
-  body('email').isEmail(), // есть ли в body 'email' и я является ли он email
-  body('password').isLength({ min: 5 }), // есть ли в body 'password' и чтобы он был длинее 5 символов
-  body('fullName').isLength({ min: 3 }),
-  body('avatarUrl').optional().isURL(), // есть ли в body 'avatarUrl' опционально, а если есть то проверяем является ли она URL
+  body('email', 'Неверный формат почты').isEmail(), // есть ли в body 'email' и я является ли он email
+  body('password', 'Пароль должен быть минимум 5 символов').isLength({ min: 5 }), // есть ли в body 'password' и чтобы он был длинее 5 символов
+  body('fullName', 'Укажите имя, не менее 3 символов').isLength({ min: 3 }),
+  body('avatarUrl', 'Неверная ссылка на аватарку').optional().isURL(), // есть ли в body 'avatarUrl' опционально, а если есть то проверяем является ли она URL
 ];
