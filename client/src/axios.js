1718554db1a4d52e3bfd159ef6454878f2471bcd
Murad <murad186992@mail.ru>
2023-06-15 17:29:33 +0300

    setLocalStorage-token

--- client/src/axios.js
+++ client/src/axios.js
@@ -4,4 +4,11 @@ const instance = axios.create({
   baseURL: 'http://localhost:3001',
 });
 
+// мидлвара которая проверяет есть токен или нет его
+instance.interceptors.request.use((config) => {
+  // при любом запросе проверяем есть ли в local storage токен
+  config.headers.Authorization = window.localStorage.getItem('token');
+  return config;
+});
+
 export default instance;
