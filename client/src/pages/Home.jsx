7aafae5ef6d6bcf42ab030983429f602b9266e57
Murad <murad186992@mail.ru>
2023-06-15 20:38:23 +0300

    admin-controlPanel

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -10,6 +10,7 @@ import { useDispatch, useSelector } from 'react-redux';
 import { fetchPosts, fetchTags } from '../redux/slices/posts';
 
 export const Home = () => {
+  const userData = useSelector((state) => state.auth.data);
   const dispatch = useDispatch();
   const { posts, tags } = useSelector((state) => state.posts);
 
@@ -17,7 +18,7 @@ export const Home = () => {
   const isTagsLoading = tags.status === 'loading';
   useEffect(() => {
     dispatch(fetchPosts());
-    dispatch(fetchTags())
+    dispatch(fetchTags());
   }, []);
 
   return (
@@ -40,7 +41,8 @@ export const Home = () => {
                 viewsCount={obj.viewsCount}
                 commentsCount={3}
                 tags={obj.tags}
-                isEditable
+                // сравниваем id, чтобы удалять и редактировать мог только тот кто создал пост
+                isEditable={userData?._id === obj.user._id}
               />
             ),
           )}
